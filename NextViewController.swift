5fe072473d8341a59106e82a646a3bc6bec0a344
prajaktaK2808 <55080987+prajaktaK2808@users.noreply.github.com>
2019-12-27 12:33:30 +0530

    Add files via upload

---
+++ NextViewController.swift
@@ -0,0 +1,59 @@
+//
+//  NextViewController.swift
+//  todolist
+//
+//  Created by Student P_08 on 17/12/19.
+//  Copyright © 2019 felix. All rights reserved.
+//
+
+import UIKit
+
+class NextViewController: UIViewController {
+    
+    
+    @IBOutlet weak var textfield1: UITextField!
+    
+    
+
+    @IBOutlet weak var textfield2: UITextField!
+    
+    
+    @IBAction func savebutton(_ sender: UIButton) {
+        let insertQuery = "insert into taskTable(taskId,taskName) values ('\(textfield1.text!)','\(textfield2.text!)')"
+        let isSuccess = DBWrapper.sharedObj.executeQuery(query: insertQuery)
+            if isSuccess
+        {
+            print("insert:success")
+        }
+        else
+        {
+            print("insert:failed")
+        }
+        navigationController?.popViewController(animated: true)
+        
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
